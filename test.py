7f465fb06e53c5ae0a0459eb97b3d32cd0567175
aldafari <aldafari@memphis.edu>
2024-02-12 13:50:59 -0600

    Replaced 'adult' with 'big boy' in first print statement

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ def age_limit
     print("How old are you?")
     age = input(int)
     if age<18:
-        print("You are not a adult")
+        print("You are not a big boy")
     else:
         print("You are not an adult.")
         
