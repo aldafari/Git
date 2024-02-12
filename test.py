76c4b915496e0f244c0c4e21f7a039d2cf63cdcd
aldafari <aldafari@memphis.edu>
2024-02-12 13:33:17 -0600

    Added hello word

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 def age_limit
+    print("Hello word!")
     print("How old are you?")
     age = input(int)
     if age<18:
         print("You are not a adult")
+        
     else:
         print("You are not an adult.")
         
