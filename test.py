e38df2ed3779a23070f7f6742ef62d48cefca901
aldafari <aldafari@memphis.edu>
2024-02-12 13:20:00 -0600

    Turned program into a function.

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-print("How old are you?")
-age = input(int)
-if age<18:
-    print("You are not a adult")
-else:
-    print("You are an adult.")
+def age_limit
+    print("How old are you?")
+    age = input(int)
+    if age<18:
+        print("You are not a adult")
+    else:
+        print("You are an adult.")
+        
